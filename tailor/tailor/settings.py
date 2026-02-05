0fc3b62fbc168ef5b2d545fec627dae62ea62940
abhirajdas06 <abhirajdas57@gmail.com>
2026-02-05 16:38:29 +0530

    fix: Set DEBUG to False for production readiness and optimize static file settings

--- tailor/tailor/settings.py
+++ tailor/tailor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e(98hx6i^bn4d#&rqrh=(vdvx0^1h*29)6vu^dtm#*9a31io%o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -31,17 +31,17 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
+
     'api.apps.ApiConfig',
     'django.contrib.humanize',
     'importer',
-    
 ]
 
 MIDDLEWARE = [
@@ -171,13 +171,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
 ]
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
